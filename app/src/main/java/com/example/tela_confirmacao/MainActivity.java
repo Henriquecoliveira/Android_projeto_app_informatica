274e20d1a7b495a1959f7054675f8bf242be421f
User <ja1miguelsantus@gmail.com>
2026-04-03 13:10:31 -0300

    teste

---
+++ app/src/main/java/com/example/tela_confirmacao/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.tela_confirmacao;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tela_Confirmacao), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void confirmar(View view){
+        Toast.makeText(this, "Pedido confirmado!!", Toast.LENGTH_SHORT).show();
+    }
+}
