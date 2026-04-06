01b756e75501828f409e6488629df584ac54f617
IgorLacerdaa <lacigor65@gmail.com>
2026-04-06 20:41:29 -0300

    feat: merged tela_henrique

--- app/src/main/java/com/example/app_informatica/MainActivity.java
+++ app/src/main/java/com/example/app_informatica/MainActivity.java
@@ -13,13 +13,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CarrinhoActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.carrinho_main);
+        setContentView(R.layout.activity_main);
         montarRecyclerView();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
