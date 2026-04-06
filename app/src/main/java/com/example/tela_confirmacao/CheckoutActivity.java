4d5b47921009d575ae3ecfcee7faab52efba3edd
IgorLacerdaa <lacigor65@gmail.com>
2026-04-06 20:20:44 -0300

    rename class to checkout

--- app/src/main/java/com/example/tela_confirmacao/CheckoutActivity.java
+++ app/src/main/java/com/example/tela_confirmacao/CheckoutActivity.java
@@ -10,13 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class CheckoutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.checkout_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tela_Confirmacao), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
